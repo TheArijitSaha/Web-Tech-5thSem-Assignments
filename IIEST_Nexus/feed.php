d6ace54b66e12d0c2d5ad5e81fa78d88a0f5e8b4
adityaraj75 <39653116+adityaraj75@users.noreply.github.com>
2019-10-23 18:20:01 +0530

    Revert "tiny things"

--- IIEST_Nexus/feed.php
+++ IIEST_Nexus/feed.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 
 <?php
-    require_once('classes/LoginClass.php');
-    require_once('classes/User.php');
-    require_once('classes/variables.php');
-    require_once('classes/NexusNav.php');
-    require_once('classes/DataBase.php');
+require_once('classes/LoginClass.php');
+require_once('classes/User.php');
+require_once('classes/variables.php');
+require_once('classes/NexusNav.php');
+require_once('classes/DataBase.php');
 
-    $logged_in_id = LoginClass::isLoggedIn();
+$logged_in_id = LoginClass::isLoggedIn();
 
-    if (! $logged_in_id)
-    {
-        header("Location: welcome.php");
-        exit();
-    }
+if (! $logged_in_id)
+{
+    header("Location: welcome.php");
+    exit();
+}
 
-    $current_user = new User($logged_in_id);
+$current_user = new User($logged_in_id);
 
 ?>
 
@@ -28,63 +28,8 @@
         <link rel="stylesheet" href="static/css/feed_master.css">
     </head>
     <body>
-        <!-- Navbar Begins-->
+        <!-- Navbar -->
         <?php echo NexusNav::insertNavbar(); ?>
-        <!-- Navbar Ends-->
-
-        <div class="container-fluid">
-            <div class="row">
-                <!-- Left Column Begin-->
-                <div class="col-sm-3">
-                    <div class="container">
-                        <div class="card">
-                            <div class="card-header">
-                                <div class="form-group">
-                                    <textarea class="form-control" name="postbody" id="postContent" placeholder="Enter your thoughts here"></textarea>
-                                </div>
-                                <div class="form-group">
-                                    <button type="button" class="form-control btn btn-primary" name="createPost" id="postCreateBtn">Share</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <p>Welcome to your feed <?php //echo $current_user->getName(); ?></p>
-
-                </div>
-                <!-- Left Column End -->
-
-                <!-- Middle Column Begin-->
-                <div class="col-sm-7">
-                    <!-- Posting -->
-                    <div class="container">
-                        <div class="card">
-                            <div class="card-header">
-                                <div class="form-group">
-                                    <textarea class="form-control" name="postbody" id="postContent" placeholder="Enter your thoughts here"></textarea>
-                                </div>
-                                <div class="form-group">
-                                    <button type="button" class="form-control btn btn-primary" name="createPost" id="postCreateBtn">Share</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Posting Ends -->
-
-                    <!-- Post List: -->
-                    <div class="container">
-                        <div class="postBox">
-
-                        </div>
-                    </div>
-
-                    <!-- Post List Ends -->
-
-
-                </div>
-                <!-- Middle Column End -->
-
-            </div>
-        </div>
 
         <!-- <div class="container">
             <div class="jumbotron">
@@ -93,6 +38,29 @@
         </div> -->
 
 
+        <!-- Posting -->
+        <div class="container">
+            <div class="card">
+                <div class="card-header">
+                    <div class="form-group">
+                        <textarea class="form-control" name="postbody" id="postContent" placeholder="Enter your thoughts here"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button type="button" class="form-control btn btn-primary" name="createPost" id="postCreateBtn">Share</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Posting Ends -->
+
+        <!-- Post List: -->
+        <div class="container">
+            <div class="postBox">
+
+            </div>
+        </div>
+
+        <!-- Post List Ends -->
 
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
--- IIEST_Nexus/welcome.php
+++ IIEST_Nexus/welcome.php
@@ -2,15 +2,15 @@
 
 <?php
 
-    require_once('classes/LoginClass.php');
-    require_once('classes/variables.php');
-    require_once('classes/NexusNav.php');
+require_once('classes/LoginClass.php');
+require_once('classes/variables.php');
+require_once('classes/NexusNav.php');
 
-    if (LoginClass::isLoggedIn())
-    {
-        header("Location: feed.php");
-        exit();
-    }
+if (LoginClass::isLoggedIn())
+{
+    header("Location: feed.php");
+    exit();
+}
 
 ?>
 
@@ -36,16 +36,16 @@
                 <div class="col-sm-8">
                     <div class="container">
                         <h3>Log In</h3>
-                        <form id="LogInForm">
+                        <form id="LogInForm" method="POST">
                             <div class="row">
                                 <div class="col-sm-4">
-                                    <input class="form-control" id="LoginUsername" type="text" name="loginuser" placeholder="Enter Email ID">
+                                    <input class="form-control" id="LoginUsername" type="text" name="loginuser" placeholder="Enter Email ID" required>
                                 </div>
                                 <div class="col-sm-4">
-                                    <input class="form-control" id="LoginPassword" type="password" name="loginpass" placeholder="Enter Password">
+                                    <input class="form-control" id="LoginPassword" type="password" name="loginpass" placeholder="Enter Password" required>
                                 </div>
                                 <div class="col-sm-4 form-group">
-                                    <button type="button" class="btn btn-primary" name="login" id="loginBtn">Log In</button>
+                                    <input class='btn btn-primary' type="submit" name="login" value="Log In" formaction="login.php">
                                 </div>
                             </div>
                         </form>
@@ -88,9 +88,8 @@
         </div>
 
 
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <script src="static/js/login_script.js"></script>
     </body>
 </html>
